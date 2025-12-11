e5f3ce11aea86a8d16f51841e2c8695054381e1f
Ronyeri Marinho <76921589+ronyeri-marinho@users.noreply.github.com>
2025-12-11 16:22:02 -0300

    ✨ feat: Beecrowd 1050

---
+++ beecrowd1050.clj
@@ -0,0 +1,12 @@
+(def cities
+  {61 "Brasilia"
+   71 "Salvador"
+   11 "Sao Paulo"
+   21 "Rio de Janeiro"
+   32 "Juiz de Fora"
+   19 "Campinas"
+   27 "Vitoria"
+   31 "Belo Horizonte"})
+
+(let [code (Integer/parseInt (read-line))]
+  (println (get cities code "DDD nao cadastrado")))
